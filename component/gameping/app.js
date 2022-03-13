76d5690ae789a25140701292039f4260092182a0
kishor.sarkar <kishor.sarkar@optimizeitsystems.com>
2022-03-13 22:29:51 +0530

    Gameboard Updated

--- component/gameping/app.js
+++ component/gameping/app.js
@@ -1,9 +1,9 @@
-const p1btn = document.querySelector("#p1btn");
+/* const p1btn = document.querySelector("#p1btn");
 const p2btn = document.querySelector("#p2btn");
-const resetData = document.querySelector("#reset");
+
 
 const p1display = document.querySelector("#p1display");
-const p2display = document.querySelector("#p2display");
+const p2display = document.querySelector("#p2display"); */
 
 let myselect = document.querySelector("#my-select");
 for (let index = 1; index < 21; index++) {
@@ -21,12 +21,40 @@ myselect.addEventListener("change", function () {
   reset();
 });
 
-let p1score = 0;
-let p2score = 0;
+const resetData = document.querySelector("#reset");
+
+let p1 = {
+  score: 0,
+  button: document.querySelector("#p1btn"),
+  display: document.querySelector("#p1display"),
+};
+let p2 = {
+  score: 0,
+  button: document.querySelector("#p2btn"),
+  display: document.querySelector("#p2display"),
+};
+
+function updateScore(player, oppnent) {
+  if (!isGOver) {
+    player.score += 1;
+    if (player.score === winScore) {
+      isGOver = true;
+      player.display.className = "winner";
+      oppnent.display.className = "looser";
+      player.button.Disabled = true;
+      oppnent.button.Disabled = true;
+    }
+    player.display.textContent = player.score;
+    console.log(player.score);
+  }
+}
+
+/* let p1score = 0;
+let p2score = 0; */
 let isGOver = false;
 
-p1btn.addEventListener("click", function () {
-  if (!isGOver) {
+p1.button.addEventListener("click", function () {
+  /* if (!isGOver) {
     p1score += 1;
     if (p1score === winScore) {
       isGOver = true;
@@ -34,14 +62,14 @@ p1btn.addEventListener("click", function () {
       p2display.className = "looser";
       p1btn.Disabled = true;
       p2btn.Disabled = true;
-
     }
     p1display.textContent = p1score;
     console.log(p1score);
-  }
+  } */
+  updateScore(p1, p2);
 });
-p2btn.addEventListener("click", function () {
-  if (!isGOver) {
+p2.button.addEventListener("click", function () {
+  /*  if (!isGOver) {
     p2score += 1;
     if (p2score === winScore) {
       isGOver = true;
@@ -49,11 +77,11 @@ p2btn.addEventListener("click", function () {
       p1display.className = "looser";
       p1btn.Disabled = true;
       p2btn.Disabled = true;
-
     }
     p2display.textContent = p2score;
     console.log(p2score);
-  }
+  } */
+  updateScore(p2, p1);
 });
 
 if (!isGOver) {
@@ -64,12 +92,19 @@ resetData.addEventListener("click", reset);
 
 function reset() {
   isGOver = false;
-  p1display.textContent = 0;
-  p2display.textContent = 0;
-  p1score = 0;
-  p2score = 0;
-  p1display.classList.remove("winner", "looser");
-  p2display.classList.remove("winner", "looser");
-  p1btn.Disabled = false;
-  p2btn.Disabled = false;
+
+  for (const iterator of [p1, p2]) {
+    iterator.score = 0;
+    iterator.display.textContent = 0;
+    iterator.display.classList.remove("winner", "looser");
+    iterator.button.Disabled = false;
+  }
+  /* p1.display.textContent = 0;
+  p2.display.textContent = 0;
+  p1.score = 0;
+  p2.score = 0;
+  p1.display.classList.remove("winner", "looser");
+  p2.display.classList.remove("winner", "looser");
+  p1.button.Disabled = false;
+  p1.button.Disabled = false; */
 }
