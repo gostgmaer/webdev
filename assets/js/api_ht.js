187e0008cc5d657f9d4e656ed78b7b35e5f172b2
kishor.sarkar <kishor.sarkar@optimizeitsystems.com>
2022-03-27 22:02:22 +0530

    fetch API has Been Addad

--- assets/js/api_ht.js
+++ assets/js/api_ht.js
@@ -10,12 +10,10 @@ apiReq.onload = function(){
     console.log(this.response);
 }
 
-
 apiReq.onerror= function(){
     console.log('API CALL ERROR');
     console.log(this.response);
 }
-
 
 apiReq.open('GET','https://api.tvmaze.com/search/shows?q=girls');
 apiReq.send();
@@ -24,5 +22,39 @@ const resss = apiReq.responseText;
 
 console.log(resss); */
 
+/* fetch("https://api.tvmaze.com/search/shows?q=girls")
+  .then((res) => { 
+    console.log("response..... =>     ", res);
+    return res.json();
+  })
+  .then((data) => {
+    console.log( data);
+    console.log("showing Data....=>   ", data[0].show.id);
+  })
 
-fetch('https://api.tvmaze.com/search/shows?q=girls');
+  .catch((e) => {
+    console.log("Error", e);
+  }); */
+
+const fetTvShow = async() =>{
+    const res = await fetch("https://api.tvmaze.com/search/shows?q=heros")
+    console.log(res);
+    const data= await res.json();
+    console.log(data);
+}
+
+
+
+const fetTVShow = async() =>{
+try {
+    const res = await fetch("https://api.tvmaze.com/search/shows?=heros")
+    console.log(res);
+    const data= await res.json();
+    console.log(data);
+    
+} catch (error) {
+    console.log('Something Wrong Please Try Again')
+    
+}
+   
+}
