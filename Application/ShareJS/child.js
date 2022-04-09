7f36fcd96dc46f10ca2984c5af391b8867e2b279
kishor.sarkar <kishor.sarkar@optimizeitsystems.com>
2022-04-09 19:41:52 +0530

    api call updated and with remove element

---
+++ Application/ShareJS/child.js
@@ -0,0 +1,9 @@
+const add = (x, y) => x + y;
+const PI = 3.141592653589793;
+const squre = (x) => x * x;
+
+
+
+exports.PI=PI;
+exports.add=add;
+exports.squre=squre;
---
+++ Application/ShareJS/script.js
@@ -0,0 +1,5 @@
+
+
+console.log(add(5,6));
+console.log(axios);
+
